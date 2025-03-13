179d7d9d42abcad6bf081befa69c4c6bdf207aed
Libardo-P <libardopereira@outlook.com>
2025-03-13 18:31:55 +0000

    titulo

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1>Bem-vindo ao Meu Projeto Nuxt</h1>
+      <h1>Bem-vindo ao Meu Projeto de Nuxt</h1>
       <NuxtLink to="/sobre">Ir para a página Sobre</NuxtLink>
     </div>
   </template>
