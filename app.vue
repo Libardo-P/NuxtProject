97abfaff66468e6d220562e91d44a2e6bb18e1c6
Libardo-P <libardopereira@outlook.com>
2025-03-13 18:29:32 +0000

    initial

---
+++ app.vue
@@ -0,0 +1,27 @@
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+
+<template>
+  <div>
+    <header>
+      <h1>Meu Site</h1>
+      <nav>
+        <NuxtLink to="/">Início</NuxtLink> | 
+        <NuxtLink to="/sobre">Sobre</NuxtLink>
+      </nav>
+    </header>
+
+    <main>
+      <NuxtPage />  <!-- Aqui aparecem as páginas -->
+    </main>
+
+    <footer>
+      <p>© 2025 Meu Site</p>
+    </footer>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Bem-vindo ao Meu Projeto Nuxt</h1>
+      <NuxtLink to="/sobre">Ir para a página Sobre</NuxtLink>
+    </div>
+  </template>
+  
